6f0cf9dfeec208333d8401ad97534d785488b580
fabian-n-gude-systems <support@gude-systems.com>
2022-10-11 14:51:43 +0200

    - now using python logging - added progress bar to gblib.py - adapted to python naming convention - moved static variables to deviceValues.py - updated README.md - added annotations to upload.py - fixed version.ini

--- gude/sensor.py
+++ gude/sensor.py
@@ -9,14 +9,14 @@ class SensorVector:
     def __str__(self):
         return f"{self[0]}.{self[1]}.{self[2]}.{self[3]}.{self[4]}"
 
-    def getTuple(self):
+    def get_tuple(self):
         return self[0], self[1], self[2], self[3], self[4]
 
-    def __init__(self, typ=0, index=0, groupIndex=0, groupMemberIndex=0, fieldIndex=0, fromStr=None):
-        if fromStr is None:
-            self.vector = [typ, index, groupIndex, groupMemberIndex, fieldIndex]
+    def __init__(self, typ=0, index=0, group_index=0, group_member_index=0, field_index=0, from_str=None):
+        if from_str is None:
+            self.vector = [typ, index, group_index, group_member_index, field_index]
         else:
-            self.vector = fromStr.split('.')
+            self.vector = from_str.split('.')
 
 
 class SensorField:
@@ -48,24 +48,24 @@ class SensorDev:
         102: "Powersource"
     }
 
-    def parseSensorJson(self):
+    def parse_sensor_json(self):
         descr = {}
         values = {}
 
-        jsonIndex = -1
+        json_index = -1
 
-        sensorValueCollection = []
-        for sensorValues in self.json['values']:
-            sensorValueCollection.append((sensorValues['type'], sensorValues['num']))
-        mismatch = self.sensorValueCollection != sensorValueCollection
-        self.sensorValueCollection = sensorValueCollection
+        sensor_value_collection = []
+        for sensor_values in self.json['values']:
+            sensor_value_collection.append((sensor_values['type'], sensor_values['num']))
+        mismatch = self.sensorValueCollection != sensor_value_collection
+        self.sensorValueCollection = sensor_value_collection
         if mismatch:
             return False
 
         for sensorType in self.json['descr']:
-            jsonIndex += 1
+            json_index += 1
 
-            sensorValues = self.json['values'][jsonIndex]["values"]
+            sensor_values = self.json['values'][json_index]["values"]
             st = sensorType["type"]
 
             for (si, sensorProp) in enumerate(sensorType["properties"]):
@@ -78,7 +78,7 @@ class SensorDev:
                     for (sf, fieldProp) in enumerate(sensorType["fields"]):
                         v = str(SensorVector(st, index, 0, 0, sf))
                         descr[v] = fieldProp
-                        values[v] = sensorValues[si][sf]
+                        values[v] = sensor_values[si][sf]
 
                 # complex sensor groups
                 if 'groups' in sensorType:
@@ -89,16 +89,16 @@ class SensorDev:
                             for (sf, fieldProp) in enumerate(sensorType["groups"][gi]["fields"]):
                                 v = str(str(SensorVector(st, index, gi, grm, sf)))
                                 descr[v] = fieldProp
-                                values[v] = sensorValues[si][gi][grm][sf]
+                                values[v] = sensor_values[si][gi][grm][sf]
 
         for v in descr.keys():
-            self.sensors[v] = SensorField(SensorVector(fromStr=v), descr[v], values[v])
+            self.sensors[v] = SensorField(SensorVector(from_str=v), descr[v], values[v])
 
         return True
 
-    def get(self, vectorStr=None, cached=True, cgiParams=None):
-        if cgiParams is None:
-            cgiParams = {}
+    def get(self, vector_str=None, cached=True, cgi_params=None):
+        if cgi_params is None:
+            cgi_params = {}
         if self.json_cache_ttl == 0 or self.values_updated_at + self.json_cache_ttl < time.time():
             cached = False
 
@@ -110,7 +110,7 @@ class SensorDev:
                                 self.httpDev.JSON_STATUS_HARDWARE + \
                                 self.httpDev.JSON_STATUS_MISC
 
-            status = self.httpDev.httpGetStatusJson(status_flags, cgiParams)
+            status = self.httpDev.http_get_status_json(status_flags, cgi_params)
             self.values_updated_at = time.time()
 
             if self.json['descr'] == {}:
@@ -121,43 +121,59 @@ class SensorDev:
             self.json['values'] = status['sensor_values']
 
             try:
-                cached = self.parseSensorJson()
-            except:
+                cached = self.parse_sensor_json()
+            except ValueError:
                 cached = False
 
             if not cached:
-                cgiParams = {}
-                self.json['descr'] = {}
+                cgi_params = {}
+                self.json['descr'] = dict()
                 self.sensors = {}
                 retry -= 1
 
         if not cached:
             raise ValueError("descr/values mismatch pair cannot be resolved")
 
-        if vectorStr is not None and vectorStr in self.sensors:
-            return self.sensors[vectorStr]
+        if vector_str is not None and vector_str in self.sensors:
+            return self.sensors[vector_str]
 
     # set sensor field value (enable virtual sensor mode)
-    def set(self, vectorStr, value, kalman=True):
+    def set(self, vector_str, value, kalman=True):
         virtual_sensor_cmd = {
             'cmd': self.httpDev.CGI_CMD_VIRTUAL_SENSOR,
             'vtmode': 1,
             'kalman': 1 if kalman else 0,
-            'sensor': vectorStr,
+            'sensor': vector_str,
             'value': value
         }
-        return self.get(vectorStr, cached=False, cgiParams=virtual_sensor_cmd)
+        return self.get(vector_str, cached=False, cgi_params=virtual_sensor_cmd)
 
     # release sensor field from manuel control (disable virtual sensor mode)
-    def unset(self, vectorStr):
+    def unset(self, vector_str):
         virtual_sensor_cmd = {
             'cmd': self.httpDev.CGI_CMD_VIRTUAL_SENSOR,
             'vtmode': 0,
-            'sensor': vectorStr
+            'sensor': vector_str
         }
-        return self.get(vectorStr, cached=False, cgiParams=virtual_sensor_cmd)
+        return self.get(vector_str, cached=False, cgi_params=virtual_sensor_cmd)
 
-    def updateData(self, status=None):
+    def freeze_current_val(self, vector):
+        self.set(vector, self.get(vector).value)
+
+    def freeze_all(self, cb_func=None):
+        for vector in self.sensors:
+            val = self.get(vector).value
+            if vector.startswith('20.0.1') or val is None:
+                continue
+            if cb_func is not None:
+                cb_func(vector, val)
+            self.set(vector, val)
+
+    def unfreeze_all(self):
+        for vector in self.sensors:
+            self.unset(vector)
+
+    def update_data(self, status=None):
         if status is None:
             self.get(cached=False)
             return
@@ -170,16 +186,15 @@ class SensorDev:
             self.json['descr'] = status['sensor_descr']
 
         self.values_updated_at = time.time()
-        return self.parseSensorJson()
+        return self.parse_sensor_json()
 
-    def setCacheTtl(self, ttl):
+    def set_cache_ttl(self, ttl):
         self.json_cache_ttl = ttl
 
-    def disableCache(self):
-        self.setCacheTtl(-1)
+    def disable_cache(self):
+        self.set_cache_ttl(-1)
 
-    def __init__(self, host, ssl=False, username='admin', password='admin', logLevel=HttpDevice.LOG_NONE,
-                 json=None, httpDev=None):
+    def __init__(self, host, ssl=False, username='admin', password='admin', json=None, http_dev=None):
         # 0  : no cache at all, always reload JSON
         # <0 : cache never expires
         # >0 : Cache TTL in seconds
@@ -195,16 +210,15 @@ class SensorDev:
 
         self.values_updated_at = 0
 
-        if httpDev is None:
+        if http_dev is None:
             self.httpDev = HttpDevice(host)
             self.httpDev.httpOpts['ssl'] = ssl
             self.httpDev.httpOpts['username'] = username
             self.httpDev.httpOpts['password'] = password
-            self.httpDev.setLogLevel(logLevel)
         else:
-            self.httpDev = httpDev
+            self.httpDev = http_dev
 
         if json is None:
             self.get()
         else:
-            self.updateData(json)
+            self.update_data(json)
